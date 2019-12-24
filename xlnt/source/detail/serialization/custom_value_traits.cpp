8628985632302685b7712d35d5523d8fa798a464
Jay Two <j2doll@gmail.com>
2019-12-24 23:29:39 +0900

    testing new version

--- xlnt/source/detail/serialization/custom_value_traits.cpp
+++ xlnt/source/detail/serialization/custom_value_traits.cpp
@@ -343,15 +343,15 @@ std::string to_string(pane_state state)
     default_case("frozen");
 }
 
-std::string to_string(orientation ori) // fixed by j2doll, for compatibility.
+std::string to_string(orientation orient)
 {
-    switch (ori)
+    switch (orient)
     {
-    case orientation::default_orientation:
+    case xlnt::orientation::default_orientation:
         return "default";
-    case orientation::landscape:
+    case xlnt::orientation::landscape:
         return "landscape";
-    case orientation::portrait:
+    case xlnt::orientation::portrait:
         return "portrait";
     }
     default_case("default");
